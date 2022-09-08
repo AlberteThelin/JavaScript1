892e790f0f5632b2f825f7a63ae3362ec2e82633
AlberteThelin <albe879d@edu.zealand.dk>
2022-09-08 16:59:59 -0400

    edits

---
+++ script.js
@@ -0,0 +1,9 @@
+var firstName = "Alberte";
+var lastName = "Thelin";
+var age = 21;
+
+
+alert("My name is "+firstName + " " +lastName);
+alert("I am "+age+" years old");
+
+console.log("I'm having so much fun in this class");
